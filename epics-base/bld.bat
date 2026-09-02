0c73d5c6cb30348f950f82180bbc816dd884bddb
Xiaoqiang Wang <xiaoqiangwang@gmail.com>
2026-09-02 14:30:57 +0200

    epics-base: fix CMD syntax

--- epics-base/bld.bat
+++ epics-base/bld.bat
@@ -21,21 +21,21 @@ if errorlevel 1 (
     exit /b 1
 )
 
-# deal with env export
+REM deal with env export
 mkdir %PREFIX%\etc\conda\activate.d
 mkdir %PREFIX%\etc\conda\deactivate.d
 
 set ACTIVATE=%PREFIX%\etc\conda\activate.d\epics_base.bat
 set DEACTIVATE=%PREFIX%\etc\conda\deactivate.d\epics_base.bat
 
-# set up
+REM set up
 echo @set "EPICS_BASE=%PREFIX%/epics" >> %ACTIVATE%
 echo @set "EPICS_HOST_ARCH=%EPICS_HOST_ARCH%" >> %ACTIVATE%
 
-# tear down
+REM tear down
 echo @set "EPICS_BASE=" >> %DEACTIVATE%
 echo @set "EPICS_HOST_ARCH=" >> %DEACTIVATE%
 
-# clean up after self
+REM clean up after self
 set ACTIVATE=
 set DEACTIVATE=
